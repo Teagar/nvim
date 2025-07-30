702cf5156135b108617d9f60ad57e62efd85ed73
teagar <cyanofc@gmail.com>
2025-07-30 04:05:09 -0300

    feat: add mason path && add lsp
    fix: lazy on init file

--- requires.lua
+++ requires.lua
@@ -1,5 +1,7 @@
 M.utils.setup_core = function()
+    require('core.mason-path')
     require('core.lazy')
+    require('core.lsp')
     require('config.settings')
     require('config.mappings')
 end

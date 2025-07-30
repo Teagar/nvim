2593994c1d29693937f96bba93eba0162940d172
teagar <cyanofc@gmail.com>
2025-07-30 04:14:49 -0300

    feat: add cursor centered when scroll
    refactor: add a variable opts

--- config/mappings.lua
+++ config/mappings.lua
@@ -1,8 +1,15 @@
 M.mappings = {}
+
+local opts = { noremap = true, silent = true }
+
 M.mappings.core = function ()
   -- Copy and paste to system clipboard
-  keymap('n', 'y', '"+y', { noremap = true, silent = true })
-  keymap('n', 'p', '"+p', { noremap = true, silent = true })
+  keymap('n', 'y', '"+y', opts)
+  keymap('n', 'p', '"+p', opts)
+
+  -- Keep cursor centered when scrolling
+  keymap("n", "<C-d>", "<C-d>zz", opts)
+  keymap("n", "<C-u>", "<C-u>zz", opts)
 end
 
 M.mappings.core()

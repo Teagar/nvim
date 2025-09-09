14b8aa0e1e5b73ac1afd76f027de5759166927eb
teagar <cyanofc@gmail.com>
2025-09-09 19:15:31 -0300

    WIP

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -9,9 +9,9 @@ M.config.mappings = {
   end,
 
   insert = function ()
-    keymap.set('n', 'op', 'o<Esc>k', 	{ noremap = true, silent = true, desc = "Add an empty line below" })
-    keymap.set('n', 'oi', 'O<Esc>j', 	{ noremap = true, silent = true, desc = "Add an empty line above" })
-    keymap.set('n', 'oo', 'A<CR>', 	{ noremap = true, silent = true, desc = "Insert above" })
+    keymap.set('n', 'op', 'o<Esc>k',    { noremap = true, silent = true, desc = "Add an empty line below" })
+    keymap.set('n', 'oi', 'O<Esc>j',    { noremap = true, silent = true, desc = "Add an empty line above" })
+    keymap.set('n', 'oo', 'A<CR>',      { noremap = true, silent = true, desc = "Insert above" })
   end,
 
   diagnostic = function ()
@@ -26,16 +26,16 @@ M.config.mappings = {
       vim.diagnostic.config({ virtual_text = new_config })
     end
 
-    keymap.set('n', '<leader>dl', toggleVirtualLines,	{ noremap = true, silent = true, desc = 'Toggle diagnostic virtual lines' })
-    keymap.set('n', '<leader>dt', toggleVirtualText,	{ noremap = true, silent = true, desc = 'Toggle diagnostic virtual text' })
+    keymap.set('n', '<leader>dl', toggleVirtualLines,   { noremap = true, silent = true, desc = 'Toggle diagnostic virtual lines' })
+    keymap.set('n', '<leader>dt', toggleVirtualText,    { noremap = true, silent = true, desc = 'Toggle diagnostic virtual text' })
 
   end,
 
   buffer_control = function ()
-    keymap.set('n', 'tn', ':bnext<CR>', 	 { noremap = true, silent = true, desc = 'Go to next buffer' })
-    keymap.set('n', 'tp', ':bprevious<CR>', 	 { noremap = true, silent = true, desc = 'Go to previous buffer' })
-    keymap.set('n', 'td', ':bdelete<CR>', 	 { noremap = true, silent = true, desc = 'Delete the current buffer' })
-    keymap.set('n', 'tad', ':bufdo bd<CR>', 	 { noremap = true, silent = true, desc = 'Delete all buffers' })
+    keymap.set('n', 'tn', ':bnext<CR>',          { noremap = true, silent = true, desc = 'Go to next buffer' })
+    keymap.set('n', 'tp', ':bprevious<CR>',      { noremap = true, silent = true, desc = 'Go to previous buffer' })
+    keymap.set('n', 'td', ':bdelete<CR>',        { noremap = true, silent = true, desc = 'Delete the current buffer' })
+    keymap.set('n', 'tad', ':bufdo bd<CR>',      { noremap = true, silent = true, desc = 'Delete all buffers' })
     keymap.set('n', '<leader>b', ':buffers<CR>', { noremap = true, silent = true, desc = 'Show all buffers' })
   end
 
--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,28 +1,40 @@
 M.config.settings = function ()
   g.mapleader = ' '
-  opt.wrap = false		-- Break the line, if true, just in view mode
-  opt.scrolloff = 8		-- Minimal number of screen lines to keep above and below the cursor
-  opt.guicursor = ""		-- Desactivate the changes of the cursor
+  g.netrw_banner = 0            -- Remove netrw banner for cleaner looking
+  opt.wrap = false              -- Break the line, if true, just in view mode
+  opt.scrolloff = 8             -- Minimal number of screen lines to keep above and below the cursor
+  opt.guicursor = ""            -- Desactivate the changes of the cursor
   opt.fillchars = {
-    eob = " ",			-- Empty lines at the end of a buffer
+    eob = " ",                  -- Empty lines at the end of a buffer
     fold = " ",
     foldopen = icons.ui.ChevronShortDown,
     foldsep = icons.ui.BoldLineLeft,
     foldclose = icons.ui.ChevronShortRight
   }
 
+  opt.list = true               -- Use special characters to represent things like tabs or trailing spaces
+  opt.listchars = {
+    tab = icons.ui.Blank..
+      icons.ui.Blank..
+      icons.ui.LineLeft,
+    trail = icons.ui.Dot,
+    extends = icons.ui.DoubleChevronRight,
+    precedes = icons.ui.DoubleChevronLeft,
+  }
+
   opt.cursorline = true
-  opt.cursorlineopt = "number"	-- Set the type of cursor line (number|line|both)
+  opt.cursorlineopt = "number"  -- Set the type of cursor line (number|line|both)
 
   opt.completeopt = { "menu", "menuone", "noselect" }
+  opt.pumheight = 10            -- Max height of completion menu
 
-  opt.foldlevel = 99
+  opt.foldlevel = 99            -- Start editing with all folds opened
   opt.foldlevelstart = 99
-  opt.foldmethod = "expr"
-  opt.updatetime = 100		-- Update time in ms(miliseconds)
+  opt.foldmethod = "expr"       -- Use tree-sitter for folding method
+  opt.updatetime = 100          -- Update time in ms(miliseconds)
 
-  opt.shiftwidth = 2		-- Number of spaces in a tab
-  opt.smartindent = true	-- Do smart autoindenting when starting a new line
+  opt.shiftwidth = 2            -- Number of spaces in a tab
+  opt.smartindent = true        -- Do smart autoindenting when starting a new line
 
   opt.splitbelow = true
   opt.splitright = true
@@ -31,12 +43,12 @@ M.config.settings = function ()
 
   opt.title = true
 
-  wo.number = true 		-- Set numbers line
+  wo.number = true              -- Set numbers line
 
-  g.loaded_perl_provider = 0	-- Disable perl
-  g.loaded_ruby_provider = 0	-- Disable ruby
+  g.loaded_perl_provider = 0    -- Disable perl
+  g.loaded_ruby_provider = 0    -- Disable ruby
 
-  vim.loader.enable() 		-- Adds the Lua loader using the byte-compilation cache 
+  vim.loader.enable()           -- Adds the Lua loader using the byte-compilation cache 
 end
 
 M.config.settings()

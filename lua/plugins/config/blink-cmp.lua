a118ac677bda6bee5c9efce4e715a1f6ab73f196
teagar <cyanofc@gmail.com>
2026-02-07 07:58:59 -0300

    refactor: better autocomplete config

--- lua/plugins/config/blink-cmp.lua
+++ lua/plugins/config/blink-cmp.lua
@@ -7,15 +7,30 @@ return {
 		},
 	},
 
-	keymap = { preset = 'default' },
+  keymap = {
+    -- none|default
+    preset = 'default',
+
+    -- ['<Tab>'] = { 'show_and_insert_or_accept_single' },
+    -- ['<S-Tab>'] = { 'show_and_insert_or_accept_single', 'select_prev' },
+
+    ['<C-space>'] = { 'show', 'hide' },
+
+    ['<C-n>'] = { 'select_next', 'fallback' },
+    ['<C-p>'] = { 'select_prev', 'fallback' },
+
+    ['<C-y>'] = { 'select_and_accept', 'fallback' },
+    ['<C-e>'] = { 'cancel', 'fallback' },
+  },
 
 	completion = {
+		ghost_text = { enabled = true, show_with_menu = false },
 		documentation = { auto_show = true, auto_show_delay_ms = 500 },
 		keyword = { range = 'full' },	-- prefix|full
 		accept = { auto_brackets = { enabled = false }, },
-		list = { selection = { preselect = false, auto_insert = true } },
+		list = { selection = { preselect = true, auto_insert = false } },
 		menu = {
-			auto_show = true,
+			auto_show = false,
 			draw = {
 				columns = {
 					{ "label", "label_description", gap = 1 },
